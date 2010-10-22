af2993847e4a739083a7c9a1a29e31270862247b
thorlist <thorlist@41b1a1b8-9ad1-474a-a649-7d62dcce9156>
2010-10-22 19:35:50 +0000

    - added setup for Munin PC   (temporary until we build a more permanent setup)
    git-svn-id: https://subversion.assembla.com/svn/ThoughtSpace/Loom/trunk@13 41b1a1b8-9ad1-474a-a649-7d62dcce9156

---
+++ setup_munin.bat
@@ -0,0 +1,7 @@
+@bin\junction.exe include\Core   Core\src
+@bin\junction.exe include\Weaver Weaver\src
+@bin\junction.exe include\MoMa   MoMa\src
+@bin\junction.exe Weaver\dependencies\win32\Ogre   d:\OgreSDK
+@bin\junction.exe Weaver\dependencies\win32\Qt     c:\Qt\4.7.0
+@bin\junction.exe MoMa\dependencies\win32\Ogre     d:\OgreSDK
+@bin\junction.exe MoMa\dependencies\win32\Qt       c:\Qt\4.7.0
