484257803dc7b4afe788dc30dd3ad03fbb0b1c56
tamas.rabel <tamas.rabel@41b1a1b8-9ad1-474a-a649-7d62dcce9156>
2010-12-23 21:18:25 +0000

    Camera controls
    git-svn-id: https://subversion.assembla.com/svn/ThoughtSpace/Loom/trunk@69 41b1a1b8-9ad1-474a-a649-7d62dcce9156

--- OgreApp/src/Qt/Ogre/cQScene.cpp
+++ OgreApp/src/Qt/Ogre/cQScene.cpp
@@ -2,9 +2,17 @@
 #include <Core/Event/cDispatcherHub.h>
 #include <OgreApp/Qt/Ogre/Event/Responders/cOgreResponderOnRender.h>
 
+#define _USE_MATH_DEFINES
+#include <math.h>
+
 #pragma warning( push )
 #pragma warning( disable: 4189 )	// local variable is initialized but not referenced
 #include <Ogre/Ogre.h>
+#pragma warning( pop )
+
+#pragma warning( push )
+#pragma warning( disable: 4512 )	// 'QLatin1Literal' : assignment operator could not be generated
+#include <QtGui/QtGui>
 #pragma warning( pop )
 
 using namespace Loom::OgreApp;
@@ -13,9 +21,16 @@ using Loom::OgreApp::cOgreResponderOnRender;
 
 /************************************************************************/
 cQScene::cQScene( QWidget *iParent )
-: cQOgre( iParent )
+: cQOgre( iParent ), mZoom( 200 ), mCameraAngle( 0, 0 ), mCameraTarget( 0, 0, 0 )
+, mCameraAngularVelocity( 0, 0 ), mCameraVelocity( 0, 0, 0 )
 /************************************************************************/
 {
+	for ( int i=0; i<KEY_NUM; i++ )
+	{
+		mKeyStates[i] = false;
+	}
+
+	setFocusPolicy( Qt::ClickFocus );
 }
 
 /**********************************************************************/
@@ -50,6 +65,8 @@ bool cQScene::InitPost( void )
 	mViewport->setBackgroundColour( vBGColor );
 //	mScene->setFog( Ogre::FOG_LINEAR, vBGColor, 0.001f, 0, 1 );
 
+	mLastUpdate = GetTickCount();
+
 	return true;
 }
 
@@ -61,6 +78,167 @@ void cQScene::RenderFrame( void )
 	
 //    ASSERTTXT( mWindow, "Main window is invalid" );
 //    ASSERTTXT( mCamera, "Main camera is invalid" );
-    
+
+	UpdateCamera();
+
     update();
+}
+
+/************************************************************************/
+void Loom::OgreApp::cQScene::mousePressEvent( QMouseEvent *iEvent )
+/************************************************************************/
+{
+	mLastMousePos = iEvent->pos();
+}
+
+/************************************************************************/
+void Loom::OgreApp::cQScene::mouseReleaseEvent( QMouseEvent *iEvent )
+/************************************************************************/
+{
+}
+
+/************************************************************************/
+void Loom::OgreApp::cQScene::mouseMoveEvent( QMouseEvent *iEvent )
+/************************************************************************/
+{
+	QPointF vDelta = iEvent->pos() - mLastMousePos;
+	if ( vDelta.manhattanLength() < 2 ) return;
+	vDelta *= -0.001f;
+	mLastMousePos = iEvent->pos();
+
+	Rotate( vDelta.x(), vDelta.y() );
+//	Move( vDelta.x(), vDelta.y() );
+}
+
+/************************************************************************/
+void Loom::OgreApp::cQScene::wheelEvent( QWheelEvent *iEvent )
+/************************************************************************/
+{
+	float vDelta = iEvent->delta(); // - mLastMousePos;
+	vDelta *= -0.0004f;
+	Zoom( vDelta );
+}
+
+/**********************************************************************/
+void Loom::OgreApp::cQScene::UpdateCamera( void )
+/**********************************************************************/
+{
+	DWORD vTime = GetTickCount();
+	float vEllapsed = ( vTime - mLastUpdate ) * 0.001f;
+	mLastUpdate = vTime;
+
+	mCameraVelocity *= powf( 0.1f, vEllapsed  );
+	mCameraAngularVelocity *= powf( 0.1f, vEllapsed );
+	mCameraAngle += mCameraAngularVelocity * vEllapsed * 10;
+	if ( mCameraAngle.y < 0 ) mCameraAngle.y = 0;
+	if ( mCameraAngle.y > M_PI_4 ) mCameraAngle.y = M_PI_4;
+	mCameraAngle.x = fmodf( mCameraAngle.x, M_PI * 2 );
+
+	Ogre::Vector3 vDir( 0, 0, 1 );
+	Ogre::Matrix3 vMatrix;
+	vMatrix.FromAxisAngle( Ogre::Vector3( 1, 0, 0 ), -Ogre::Radian( mCameraAngle.y ) );
+	vDir = vDir * vMatrix;
+	vMatrix.FromAxisAngle( Ogre::Vector3( 0, 1, 0 ), -Ogre::Radian( mCameraAngle.x ) );
+	vDir = vDir * vMatrix;
+
+	Ogre::Vector3 vCenter = mCameraTarget - vDir * mZoom;
+	mCamera->setPosition( vCenter );    
+	mCamera->lookAt( mCameraTarget );
+
+	if ( mKeyStates[KEY_LEFT] )
+	{
+		mCameraTarget -= mCamera->getRight() * vEllapsed * 42;
+	}
+	if ( mKeyStates[KEY_RIGHT] )
+	{
+		mCameraTarget += mCamera->getRight() * vEllapsed * 42;
+	}
+	if ( mKeyStates[KEY_UP] )
+	{
+		mCameraTarget += mCamera->getUp() * vEllapsed * 42;
+	}
+	if ( mKeyStates[KEY_DOWN] )
+	{
+		mCameraTarget -= mCamera->getUp() * vEllapsed * 42;
+	}
+}
+
+/**********************************************************************/
+void Loom::OgreApp::cQScene::keyPressEvent( QKeyEvent *iEvent )
+/**********************************************************************/
+{
+	int vKey = iEvent->key();
+
+	switch ( vKey )
+	{
+	case Qt::Key_Left:
+		mKeyStates[KEY_LEFT] = true;
+		break;
+	case Qt::Key_Right:
+		mKeyStates[KEY_RIGHT] = true;
+		break;
+	case Qt::Key_Up:
+		mKeyStates[KEY_UP] = true;
+		break;
+	case Qt::Key_Down:
+		mKeyStates[KEY_DOWN] = true;
+		break;
+	}
+
+	cQOgre::keyPressEvent( iEvent );
+}
+
+/**********************************************************************/
+void Loom::OgreApp::cQScene::keyReleaseEvent( QKeyEvent *iEvent )
+/**********************************************************************/
+{
+	int vKey = iEvent->key();
+
+	switch ( vKey )
+	{
+	case Qt::Key_Left:
+		mKeyStates[KEY_LEFT] = false;
+		break;
+	case Qt::Key_Right:
+		mKeyStates[KEY_RIGHT] = false;
+		break;
+	case Qt::Key_Up:
+		mKeyStates[KEY_UP] = false;
+		break;
+	case Qt::Key_Down:
+		mKeyStates[KEY_DOWN] = false;
+		break;
+	}
+
+	cQOgre::keyReleaseEvent( iEvent );
+}
+
+/**********************************************************************/
+void cQScene::Rotate( const float iDX, const float iDY )
+/**********************************************************************/
+{
+	mCameraAngularVelocity.x += iDX;
+	mCameraAngularVelocity.y += iDY;
+}
+
+/**********************************************************************/
+void cQScene::Move( const float iDX, const float iDY )
+/**********************************************************************/
+{
+	float vZoom = ( mCamera->getPosition() - Ogre::Vector3::ZERO ).length();
+	mCameraVelocity.x += iDX * vZoom * 0.1f;
+	mCameraVelocity.y -= iDY * vZoom * 0.1f;
+}
+
+/**********************************************************************/
+void cQScene::Zoom( const float iDelta )
+/**********************************************************************/
+{
+	/*
+	float vZoom = ( mCamera->getPosition() - Ogre::Vector3::ZERO ).length();
+	vZoom *= powf( 2, iDelta );
+	mCamera->setPosition( Ogre::Vector3::ZERO - mCamera->getDirection() * vZoom );
+	mCamera->lookAt( Ogre::Vector3::ZERO );                
+	*/
+	mZoom *= powf( 2, iDelta );
 }
--- OgreApp/src/Qt/Ogre/cQScene.h
+++ OgreApp/src/Qt/Ogre/cQScene.h
@@ -1,18 +1,54 @@
 #pragma once
 #include <OgreApp/Qt/Ogre/cQOgre.h>
+#include <Ogre/OgreVector2.h>
+#include <Ogre/OgreVector3.h>
 
 BEGIN_NAMESPACE( OgreApp )
 
 class OGREAPP_API cQScene : public cQOgre
 {
 protected:
+	enum eKey
+	{
+		KEY_LEFT = 0,
+		KEY_RIGHT,
+		KEY_UP,
+		KEY_DOWN,
+		KEY_NUM,
+	};
+
+protected:
+	bool mKeyStates[KEY_NUM];
+
+	// Camera controls ( TODO: Create external class for camera handling )
+	QPoint mLastMousePos;
+	Ogre::Vector2 mCameraAngularVelocity;        
+	Ogre::Vector2 mCameraAngle;        
+	Ogre::Vector3 mCameraVelocity;
+	Ogre::Vector3 mCameraTarget;
+	float mZoom;
+	DWORD mLastUpdate;
+
+	void Rotate( const float iDX, const float iDY );
+	void Move( const float iDX, const float iDY );
+	void Zoom( const float iDelta );
+	void UpdateCamera( void );
 
 	virtual bool InitResources( void );
 	virtual bool InitPost( void );
+
 public:
 	cQScene( QWidget *iParent = NULL );
 
 	virtual void RenderFrame( void );
+
+	// QWidget methods
+	virtual void mousePressEvent( QMouseEvent *iEvent );
+	virtual void mouseReleaseEvent( QMouseEvent *iEvent );
+	virtual void mouseMoveEvent( QMouseEvent *iEvent );
+	virtual void wheelEvent( QWheelEvent *iEvent );
+	virtual void keyPressEvent( QKeyEvent *iEvent );
+	virtual void keyReleaseEvent( QKeyEvent *iEvent );
 };
 
 END_NAMESPACE()
