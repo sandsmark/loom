b34bcfabb4fea241a3d6c21e42a248a5aa835785
tamas.rabel <tamas.rabel@41b1a1b8-9ad1-474a-a649-7d62dcce9156>
2011-03-03 21:16:56 +0000

    Multithread-safe circle creation
    git-svn-id: https://subversion.assembla.com/svn/ThoughtSpace/Loom/trunk@88 41b1a1b8-9ad1-474a-a649-7d62dcce9156

--- MoMa/src/Entity/cCircle.cpp
+++ MoMa/src/Entity/cCircle.cpp
@@ -2,17 +2,104 @@
 #include <MoMa/Entity/cPCircle.h>
 #include <Ogre/OgreBillboardSet.h>
 #include <Ogre/OgreBillboard.h>
+#include <Core/Module/cModuleManager.h>
+#include <OgreApp/Qt/Ogre/Event/Responders/cOgreResponderOnRender.h>
 
 using namespace Loom::MoMa;
+using Loom::Core::cModuleManager;
+using Loom::OgreApp::cOgreResponderOnRender;
 
 /************************************************************************/
 cCircle::cCircle( cPCircle *iProto )
-: mPrototype( iProto )
+: mPrototype( iProto ), mInited( false )
+/************************************************************************/
+{
+	cOgreResponderOnRender::Get().AddListener( *this );
+}
+
+/************************************************************************/
+void cCircle::SetColour( const Ogre::ColourValue &iColour )
+/************************************************************************/
+{
+	mColour = iColour;
+	mDirty = true;
+}
+
+/************************************************************************/
+const Ogre::ColourValue& cCircle::GetColour( )
+/************************************************************************/
+{
+	return mColour;
+}
+
+
+/************************************************************************/
+void Loom::MoMa::cCircle::SetSkew( const float iSkew )
+/************************************************************************/
+{
+	mPosition.z = iSkew;
+	mDirty = true;
+}
+
+/************************************************************************/
+void Loom::MoMa::cCircle::SetRotation( const Ogre::Radian &iRotation )
+/************************************************************************/
+{
+	mRotation = iRotation;
+	mDirty = true;
+}
+
+/************************************************************************/
+void Loom::MoMa::cCircle::SetSize( const Ogre::Vector2 &iSize )
+/************************************************************************/
+{
+	mDimensions = iSize;
+	mDirty = true;
+}
+
+/************************************************************************/
+void Loom::MoMa::cCircle::SetPosition( const Ogre::Vector2 &iPosition )
+/************************************************************************/
+{
+	mPosition.x = iPosition.x;
+	mPosition.y = iPosition.y;
+	mDirty = true;
+}
+
+/************************************************************************/
+void cCircle::OnRender( void )
+/************************************************************************/
+{
+	if ( !mInited )
+	{
+		Init();
+		mInited = true;
+	}
+
+	if ( mDirty )
+	{
+		mBillboard->setColour( mColour );
+		mBillboard->setPosition( mPosition );
+		mBillboard->setRotation( mRotation );
+		mBillboard->setDimensions( mDimensions.x, mDimensions.y );
+		mDirty = false;
+	}
+}
+
+/************************************************************************/
+void cCircle::Init( void )
 /************************************************************************/
 {
 	Ogre::BillboardSet *vBillboardSet = mPrototype->GetBillboardSet();
 //	mBillboard = vBillboardSet->createBillboard( Ogre::Math::RangeRandom( -10, 10 ), Ogre::Math::RangeRandom( -10, 10 ), Ogre::Math::RangeRandom( 0, 1000 ) );
-	mBillboard = vBillboardSet->createBillboard( Ogre::Math::RangeRandom( -10, 10 ), Ogre::Math::RangeRandom( -10, 10 ), 0.0f );
+
+	mPosition = Ogre::Vector3( Ogre::Math::RangeRandom( -10, 10 ), Ogre::Math::RangeRandom( -10, 10 ), 0.0f );
+	mDimensions = Ogre::Vector2( 16, 16 );
+	mColour = Ogre::ColourValue( 141.0f / 255.0f, 248.0f / 255.0f, 170.0f / 255.0f, 0.5f );
+	mRotation = Ogre::Radian( 0 );
+	mDirty = true;
+
+	mBillboard = vBillboardSet->createBillboard( mPosition );
 	mBillboard->setDimensions( 16, 16 );
 	mBillboard->setColour( Ogre::ColourValue( 141.0f / 255.0f, 248.0f / 255.0f, 170.0f / 255.0f, 0.5f ) );
 	mBillboards.Add( mBillboard );
@@ -27,52 +114,4 @@ cCircle::cCircle( cPCircle *iProto )
 		vParams->setNamedConstant( "mActNode", (float)iIndex );
 	} 
 	*/
-}
-
-/************************************************************************/
-void cCircle::SetColour( const Ogre::ColourValue &iColour )
-/************************************************************************/
-{
-	mBillboard->setColour( iColour );
-}
-
-/************************************************************************/
-const Ogre::ColourValue& cCircle::GetColour( )
-/************************************************************************/
-{
-	return mBillboard->getColour( );
-}
-
-
-/************************************************************************/
-void Loom::MoMa::cCircle::SetSkew( const float iSkew )
-/************************************************************************/
-{
-	Ogre::Vector3 vPos = mBillboard->getPosition();
-	vPos.z = iSkew;
-	mBillboard->setPosition( vPos );
-}
-
-/************************************************************************/
-void Loom::MoMa::cCircle::SetRotation( const Ogre::Radian &iRotation )
-/************************************************************************/
-{
-	mBillboard->setRotation( iRotation );
-}
-
-/************************************************************************/
-void Loom::MoMa::cCircle::SetSize( const Ogre::Vector2 &iSize )
-/************************************************************************/
-{
-	mBillboard->setDimensions( iSize.x, iSize.y );
-}
-
-/************************************************************************/
-void Loom::MoMa::cCircle::SetPosition( const Ogre::Vector2 &iPosition )
-/************************************************************************/
-{
-	Ogre::Vector3 vPos = mBillboard->getPosition();
-	vPos.x = iPosition.x;
-	vPos.y = iPosition.y;
-	mBillboard->setPosition( vPos );
 }
--- MoMa/src/Entity/cCircle.h
+++ MoMa/src/Entity/cCircle.h
@@ -1,27 +1,39 @@
 #pragma once
 
 #include <Core/Container/cArray.h>
+#include <OgreApp/Qt/Ogre/Event/IOgreListenerEvent.h>
+
+#include <Ogre/OgreColourValue.h>
+#include <Ogre/OgreVector2.h>
+#include <Ogre/OgreVector3.h>
 
 namespace Ogre
 {
 	class Billboard;
-	class ColourValue;
-	class Radian;
-	class Vector2;
 };
 
 using Loom::Core::cArray;
+using Loom::OgreApp::IOgreListenerEvent;
 
 BEGIN_NAMESPACE( MoMa )
 
 class cPCircle;
 
-class MOMA_API cCircle
+class MOMA_API cCircle : public IOgreListenerEvent
 {
 protected:
 	cPCircle *mPrototype;
 	cArray<Ogre::Billboard*> mBillboards;
 	Ogre::Billboard *mBillboard;
+
+	Ogre::ColourValue mColour;
+	Ogre::Vector2 mDimensions;
+	Ogre::Vector3 mPosition;
+	Ogre::Radian mRotation;
+	volatile bool mDirty;
+
+	bool mInited;
+	void Init( void );
 
 public:
 	cCircle( cPCircle *iProto );
@@ -31,11 +43,14 @@ public:
 	// alpha = 0.5 : normal display
 	// alpha = 1   : full glow
 	void SetColour( const Ogre::ColourValue &iColour );
-	const Ogre::ColourValue& GetColour( );
+	const Ogre::ColourValue& GetColour( void );
 	void SetSkew( const float iSkew );
 	void SetRotation( const Ogre::Radian &iRotation );
 	void SetSize( const Ogre::Vector2 &iSize );
 	void SetPosition( const Ogre::Vector2 &iPosition );
+
+	// IOgreListenerEvent methods
+	virtual void OnRender( void );
 };
 
 END_NAMESPACE()
